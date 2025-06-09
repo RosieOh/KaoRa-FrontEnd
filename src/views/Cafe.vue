0ed338307e3d45f570809c64f08187271c25ca25
RosieOh <dhxogns920@gmail.com>
2025-06-09 13:11:50 +0900

    FEAT : 카페 게시판 리스트 통합 뷰 추가

---
+++ src/views/Cafe.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="cafe-page">
+    <div class="container">
+      <h2 class="text-center mb-4">카페</h2>
+      <board-list
+        board-type="cafe"
+        :items="posts"
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @page-change="handlePageChange"
+        @search="handleSearch"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BoardList from '@/components/board/BoardList.vue'
+import api from '@/services/api'
+
+export default {
+  name: 'Cafe',
+  components: {
+    BoardList
+  },
+  data() {
+    return {
+      posts: [],
+      currentPage: 1,
+      totalPages: 1,
+      searchQuery: ''
+    }
+  },
+  created() {
+    this.fetchPosts()
+  },
+  methods: {
+    async fetchPosts() {
+      try {
+        const response = await api.posts.getPosts({
+          page: this.currentPage,
+          search: this.searchQuery,
+          type: 'cafe'
+        })
+        this.posts = response.data.content
+        this.totalPages = response.data.totalPages
+      } catch (error) {
+        console.error('게시글 조회 실패:', error)
+      }
+    },
+    handlePageChange(page) {
+      this.currentPage = page
+      this.fetchPosts()
+    },
+    handleSearch(query) {
+      this.searchQuery = query
+      this.currentPage = 1
+      this.fetchPosts()
+    }
+  }
+}
+</script> 
