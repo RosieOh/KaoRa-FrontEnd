8d4bf8c873e5eb020dd4954baa06470314e8323f
RosieOh <dhxogns920@gmail.com>
2025-06-09 13:01:25 +0900

    CHORE : 불필요한 파일 정리

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import AppHeader from "./layout/AppHeader";
 import AppFooter from "./layout/AppFooter";
-import Components from "./views/Components.vue";
 import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
@@ -15,15 +14,6 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "components",
-      components: {
-        header: AppHeader,
-        default: Components,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/landing",
       name: "landing",
       components: {
         header: AppHeader,
